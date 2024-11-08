7c9e3faa505a53d56694994f93a176be52767575
Rushhaabhhh <rushabhmistry16@gmail.com>
2024-11-08 17:17:19 +0530

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {Router, Routes, Route} from 'react-router-dom';
 
 import StockScrenner from './Pages/StockScreener';
 import Guide from './Pages/Guide';
 
 function App() {
   return (
-    
+    <Router>
     <Routes>
       <Route path="/" element={<StockScrenner />} />
       <Route path="/guide" element={<Guide />} />
     </Routes>
+    </Router>
 
   );
 }
